8149413080b230917f9096b92527b73848f704c1
urmila4462 <dasari.urmila4462@gmail.com>
2022-04-05 13:16:32 +0530

    [urmila]ADDED implemented head or tail using loop and printed it in flip coin simulation

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -10,3 +10,17 @@ then
 else
         echo "Its a TAIL!!"
 fi
+declare -A arr
+
+for ((i=1; i<=50; i++))
+do
+	randomCheck=$((RANDOM % 2))
+	if [ $randomCheck == 1 ]
+	then
+        	echo "HEADS"
+		((arr[H]++))
+	else
+		echo "TAILS"
+		((arr[T]++))
+	fi
+done
