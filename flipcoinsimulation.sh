d1aafbbdbd92ea2a137775a5a0db2b8668a22511
urmila4462 <dasari.urmila4462@gmail.com>
2022-04-05 13:25:35 +0530

    [urmila]ADDED implemented head or tail to check who has won 21 times in flip coin simulation

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -24,3 +24,42 @@ do
 		((arr[T]++))
 	fi
 done
+head=0
+Hcount=0
+Tcount=0
+counter=0
+
+while [ $counter -lt 21 ]
+do
+	counter=$(($counter + 1))
+	randomCheck=$((RANDOM % 2))
+	if [ $randomCheck == 1 ]
+	then
+		echo "HEADS"
+		Hcount=$(($Hcount + 1))
+
+             if [ $Hcount == 21 ]
+		then
+			break
+		fi
+       else
+		echo "TAILS"
+		Tcount=$(($Tcount + 1))
+                 if [ $Tcount == 21 ]
+		then
+			break
+		fi
+	fi
+done
+
+echo "Head = $Hcount"
+echo "Tail = $Tcount"
+ 
+if [ $Hcount -gt $Tcount ]
+then 
+   Head=$(( ($Hcount)-($Tcount) ))
+echo "Head Win by $Head times"
+else
+Tail=$(( ($Tcount)-($Hcount) ))
+echo "Tail Win by $Tail times"
+fi
